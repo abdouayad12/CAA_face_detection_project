5c5e266696b622dd45a8ee7ee366b46265542f7b
abdouayad12 <abdouayad12@gmail.com>
2021-12-21 20:27:42 +0100

    Fourth commit: Updating code

--- GUI_detect_faces.py
+++ GUI_detect_faces.py
@@ -7,6 +7,9 @@ import imutils
 import os
 import detect_faces as df
 import numpy as np
+
+
+
 def close_camera():
     global cap
     cv2.destroyAllWindows()
@@ -20,10 +23,10 @@ def change_to_camera_frame():
    detect_faces()
    #visualisation()
    FrameContainer.forget()
-
+   upload_frame.forget()
 
 def change_to_upload_frame():
-
+   upload_frame.pack( fill = "both",expand="yes")
    FrameContainer.forget()
    camera_frame.forget()
 
@@ -31,8 +34,9 @@ def change_to_main_window():
     global ret
     if ret:
         close_camera()
+    close_image()
     camera_frame.forget()
-
+    upload_frame.forget()
     FrameContainer.pack(expand="yes")
 
 
@@ -66,6 +70,34 @@ def import_file():
 
 def close_image():
     lbl.configure(image = "")
+
+
+def import_file():
+    global img
+    filepath = askopenfilename(initialdir = os.getcwd(),title="Ouvrir une image",filetypes=[('JPG file','*.jpg'),('PNG file','*.png'),('All files','*.*')])
+    img = Image.open(filepath)
+    lar, long = img.size
+    if (lar > 720):
+        if(long > 480):
+            img = img.resize((720,480), Image.ANTIALIAS)
+        else:
+            img = img.resize((720,long), Image.ANTIALIAS)
+    elif(long > 480):
+            img = img.resize((lar,480), Image.ANTIALIAS)
+
+    #Conversion de l'image en an Array
+    img = np.array(img)
+    #Appel de l'algorithme de détection de visage
+    img = df.face_detection_algo(img)
+
+    img = Image.fromarray(img)
+    img = ImageTk.PhotoImage(img)
+    lbl.configure(image = img)
+    lbl.image = img
+
+def close_image():
+    lbl.configure(image = "")
+
 
 def detect_faces():
         global ret
@@ -227,6 +259,94 @@ launchDetection=Button(buttonsContainerFrame,
 
 lblVideo = Label(camera_frame)
 lblVideo.pack(side = LEFT, padx = 30)
+
+#Le frame pour importer une image/ une vidéo
+upload_frame = Frame(mainWidget,
+
+                             bg = '#021b34',
+                             bd = 2,
+
+                             )
+
+# bouton de retour à la fenetre de départ
+returnbtn2=Button(upload_frame,
+              text="retour en arrière",
+              bg = '#021b34',
+              font=("Courier", 12, "italic"),
+              relief = GROOVE,
+              height = 2,
+              padx = 2,
+              pady = 5,
+              cursor = 'exchange',
+              command = change_to_main_window,
+              bd = 0,
+
+              )
+returnbtn2.pack(anchor = NE,
+                padx = 2,
+                pady = 5)
+
+#Frame contenant le bouton de détection et le champ de saisie
+frm = LabelFrame(upload_frame,
+                 bg = '#021b34',
+                 bd = 2,
+                 fg = '#FFFFFF',
+                 )
+frm.pack( side = RIGHT,
+          padx = 2)
+
+# bouton d'importation d'un fichier
+boutonImport=Button(frm,
+              text="Importer une image/ vidéo source",
+              bg = '#021b34',
+              font=("Courier", 12, "italic"),
+              relief = GROOVE,
+              height = 2,
+              padx = 2,
+              pady = 5,
+              cursor = 'plus',
+              command = import_file,
+              bd = 0,
+              )
+boutonImport.pack(side = TOP, pady = 50)
+
+#Titre -> choix du nombre d'étudiants total
+labelStudentNB2 = Label(frm,
+                       text="choix du nombre total d'étudiants",
+                       pady = 5,
+                       bg = '#021b34',
+                       fg = '#FFFFFF',
+                       font=("Courier", 12, "italic"),
+                       )
+labelStudentNB2.pack(side = TOP, pady = 10)
+
+
+#Choix du nombre d'étudiants total
+studentsNB2 = IntVar()
+studentsNB2.set("saisissez le nombre d'étudiants de la promo!")
+entree = Entry(frm,
+               )
+entree.pack( pady = 10)
+
+# bouton pour lancer l'algorithme de détection du nombre d'absents
+launchDetection=Button(frm,
+              text="Lancer la détection",
+              bg = '#021b34',
+              font=("Courier", 12, "italic"),
+              relief = GROOVE,
+              height = 2,
+              padx = 2,
+              pady = 5,
+              cursor = 'exchange',
+              command = popup,
+              bd = 0,
+              ).pack(side = BOTTOM, padx = 30)
+
+#Label qui va contenir l'image importée
+lbl = Label(upload_frame,
+            bg = '#021b34',
+            )
+lbl.pack(side = LEFT, padx = 10)
 
 
 
