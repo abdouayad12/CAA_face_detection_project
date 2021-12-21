02896cca23afb2ac12df3a2b697e672e2c8fef96
abdouayad12 <abdouayad12@gmail.com>
2021-12-21 11:19:25 +0100

    initial commit: detect faces algorithm and haarcascasde xml file

---
+++ detect_faces.py
@@ -0,0 +1,18 @@
+import cv2
+
+def face_detection_algo(frame):
+
+	# Chemin du Classifier
+	cascadeClassifierPath = 'haarcascade_frontalface_alt.xml'
+	cascadeClassifier = cv2.CascadeClassifier(cascadeClassifierPath)
+	# Conversion des images obtenues à niveaux de gris afin d'appliquer l'algorithme de détection
+	grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#Détection des visages
+	detectedFaces= cascadeClassifier.detectMultiScale(grayImage,  scaleFactor=1.1, minNeighbors=10, minSize=(20, 20))
+
+	for(x,y, width, height) in detectedFaces:
+		# Dessin d'un rectangle autour de(s) visage(s) détecté(s)
+		cv2.rectangle(frame, (x, y), (x+width, y+height), (0,255,0), 3)
+	return frame
+
+
